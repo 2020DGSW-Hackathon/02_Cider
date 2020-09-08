91dc2be068a32185fcfbfea4cd54a9d4c5864056
hyess210 <55439512+hyess210@users.noreply.github.com>
2020-09-08 15:38:02 +0900

    [ADD] Route, NavBar

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import NavBar from 'components/Common/NavBar/NavBar';
+
 const App = () => {
   return (
     <>
-      <div>React Mobx PCC default Setting JS</div>
+    <Switch>
+      <Route
+      exact path='/' component={NavBar}
+      />
+    </Switch>
     </>
   );
 };
